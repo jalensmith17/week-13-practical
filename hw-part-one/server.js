de5d51419ab2118a41a786c5d07cd3ce564765f5
Jalen <jalenms2012@gmail.com>
2023-12-17 19:34:45 -0800

    Tip Calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -5,13 +5,20 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+//greeting route
 app.get('/greeting', (req, res) => {
     res.send('Hello, stranger');
 })
 
+//greeting route with name
 app.get('/greeting/:name', (req, res) => {
     res.send(`Hello, ${req.params.name}, it's so great to see you!`);
 
+})
+
+//tip calc
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Your tip is $${req.params.total * req.params.tipPercentage / 100}`);
 })
 
 app.listen(3000, () => {
